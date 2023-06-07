9efb4d5044b729f52245ee312dc4da8ef74ac437
Matthew Montejo <montejododger@yahoo.com>
2023-06-07 12:06:37 -0700

    p

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -1,36 +1,58 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
 
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
 
-import { useTheme } from '../../context/ThemeContext';
+import { useTheme } from "../../context/ThemeContext";
 
 function Greenhouse() {
-  const {themeName, setThemeName} = useTheme()
-  console.log(themeName)
-  console.log(setThemeName)
+    const { themeName, setThemeName } = useTheme();
+    const imageSource = themeName === "day" ? dayImage : nightImage;
 
-  return (themeName === "day") ? (
-    <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
-      <LightSwitch />
-      <ClimateStats />
-    </section>
-  ) : (
-    <section>
-    <img  className='greenhouse-img'
-          src={nightImage}
-          alt='greenhouse' 
-    />
-    <LightSwitch />
-    <ClimateStats />
-    </section>
-  )
+    // const toggleTheme = () => {
+    //     const newThemeName = themeName === "day" ? "night" : "day";
+    //     setThemeName(newThemeName);
+    // };
+
+    return (
+        <section>
+            <img
+                className="greenhouse-img"
+                src={imageSource}
+                alt="greenhouse"
+            />
+            <LightSwitch />
+            <ClimateStats />
+        </section>
+    );
 }
 
 export default Greenhouse;
+
+// function Greenhouse() {
+//   // const {themeName, setThemeName} = useTheme()
+//   // console.log(themeName)
+//   // console.log(setThemeName)
+
+//   // return (themeName === "day") ? (
+//   //   <section>
+//   //     <img  className='greenhouse-img'
+//   //           src={dayImage}
+//   //           alt='greenhouse'
+//   //     />
+//   //     <LightSwitch />
+//   //     <ClimateStats />
+//   //   </section>
+//   // ) : (
+//   //   <section>
+//   //   <img  className='greenhouse-img'
+//   //         src={nightImage}
+//   //         alt='greenhouse'
+//   //   />
+//   //   <LightSwitch />
+//   //   <ClimateStats />
+//   //   </section>
+// )
+// }
