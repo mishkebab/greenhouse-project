fca8fbd608f384626f03c53cedef57696539f4c3
misha <mishabansal7@gmail.com>
2023-06-07 14:55:07 -0700

    finished bonus

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Hygrometer/Hygrometer.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Hygrometer/Hygrometer.js
@@ -1,17 +1,35 @@
 import ReactSlider from "react-slider";
 import "./Hygrometer.css";
 import { useClimate } from "../../context/ClimateContext";
+import { useState, useEffect } from "react";
 
 function Hygrometer() {
   const {humidity, setHumidity} = useClimate();
+
+  const [goalHumidity, setGoalHumidity] = useState(humidity);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+    console.log("1 second has passed")
+    if (goalHumidity < humidity) {
+      setHumidity(humidity - 1);
+    } else if (goalHumidity > humidity) {
+      setHumidity(humidity + 1);
+    } 
+  }, 1000)
+
+  return () => {
+      clearTimeout(timer);
+  }
+}, [humidity, goalHumidity])
 
   return (
     <section>
       <h2>Hygrometer</h2>
       <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={humidity}
-        onAfterChange={(val) => {setHumidity(val)}}
+        value={goalHumidity}
+        onAfterChange={(val) => {setGoalHumidity(val)}}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Thermometer/Thermometer.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Thermometer/Thermometer.js
@@ -1,17 +1,35 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
 import { useClimate } from "../../context/ClimateContext";
+import { useState, useEffect } from "react";
 
 function Thermometer() {
   const {temp, setTemp} = useClimate();
+
+  const [goalTemp, setGoalTemp] = useState(temp);
+
+  useEffect(() => {
+      const timer = setTimeout(() => {
+      console.log("1 second has passed")
+      if (goalTemp < temp) {
+        setTemp(temp - 1);
+      } else if (goalTemp > temp) {
+        setTemp(temp + 1);
+      } 
+    }, 1000)
+  
+    return () => {
+        clearTimeout(timer);
+    }
+  }, [temp, goalTemp])
 
   return (
     <section>
       <h2>Thermometer</h2>
       <div className="actual-temp">Actual Temperature: {temp}°F</div>
       <ReactSlider
-        value={temp}
-        onAfterChange={(val) => {setTemp(val)}}
+        value={goalTemp}
+        onAfterChange={(val) => {setGoalTemp(val)}}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
